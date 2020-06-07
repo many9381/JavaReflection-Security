982df7c2a97851d6d3067f335e05c8bacc1babb7
many <many9381@gmail.com>
2020-06-07 23:06:58 +0900

    feat: 지문인증 시스템 구현
    target sdk 28 이상을 요구하는 Biometric 대신 fingerprintManager로 구현

--- app/src/main/java/choi/security/MainActivity.java
+++ app/src/main/java/choi/security/MainActivity.java
@@ -1,8 +1,12 @@
 package choi.security;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+
+import java.lang.reflect.InvocationTargetException;
+import java.security.SecureRandom;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         Security test = new Security();
-        test.FIDO(getApplicationContext(), MainActivity.this, null);
+        test.FIDO(getApplicationContext(), this, null);
+         */
+        try {
+            new Security().FIDO(getApplicationContext(), this, null);
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/choi/security/Security.java
+++ app/src/main/java/choi/security/Security.java
@@ -1,20 +1,36 @@
 package choi.security;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.KeyguardManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
 
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-;
+import java.lang.reflect.InvocationTargetException;
+import java.security.KeyStore;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 
 public class Security {
     private static final String CLASS_TAG = Security.class.getSimpleName();
     private static final String KEY_NAME = "example_key";
+    Context mContext;
+    Activity mAct;
 
     /*
         스크린샷 capture 방지
@@ -24,16 +40,136 @@ public class Security {
                 WindowManager.LayoutParams.FLAG_SECURE);
     }
 
-    Context context;
+    //private class FingerprintListener extends FingerprintManager.AuthenticationCallback {
+    private class FingerprintListener extends FingerprintManager.AuthenticationCallback {
+
+        private FingerprintManager.CryptoObject cryptoObject;
+        private Dialog dialog;
+
+        //public FingerprintListener(Dialog dialog, FingerprintManager.CryptoObject cryptoObject) {
+        public FingerprintListener(Dialog dialog, FingerprintManager.CryptoObject cryptoObject) {
+            super();
+            this.dialog = dialog;
+            this.cryptoObject = cryptoObject;
+        }
 
 
-    public void FIDO(Context context, Activity act, Object[] params) {
+        @Override
+        public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+            super.onAuthenticationSucceeded(result);
+            dialog.dismiss();
+            Toast.makeText(mAct, "인증 성공", Toast.LENGTH_LONG).show();
+            Log.i(CLASS_TAG, "인증 성공");
+        }
+
+
+        @Override
+        public void onAuthenticationFailed() {
+            super.onAuthenticationFailed();
+
+            Log.i(CLASS_TAG, "인증 실패");
+            Toast.makeText(mAct, "Not Authenticated", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+            super.onAuthenticationHelp(helpCode, helpString);
+        }
+
+        @Override
+        public void onAuthenticationError(int errorCode, CharSequence errString) {
+            super.onAuthenticationError(errorCode, errString);
+            Toast.makeText(mAct, errString, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private KeyguardManager mKeyguardManager;
+    private FingerprintManager mFingerPrintManager;
+    private KeyStore mKeyStore;
+    private KeyGenerator mKeyGenerator;
+    private FingerprintManager.CryptoObject mCryptoObj;
+
+    public void FIDO(final Context context, Activity act, Object[] params) throws IllegalAccessException, InvocationTargetException, InstantiationException {
         String FUNC_TAG = "FIDO";
 
+        this.mContext = context;
+        this.mAct = act;
+
+
         // TODO: 기능지원이 안될 시 강제종료 기능 추가
+        mKeyguardManager = mAct.getSystemService(KeyguardManager.class);
+        mFingerPrintManager = mAct.getSystemService(FingerprintManager.class);
 
-        Toast.makeText(act, "tasdf",Toast.LENGTH_SHORT).show();
 
+        LayoutInflater myInflater = LayoutInflater.from(context);
+        layout =  myInflater.inflate(R.layout.fragment_fingerprint, null, false);
+
+        createKey();
+        showFingerPrintDialog();
+
+    }
+
+    private View layout;
+    CancellationSignal cancellationSignal = new CancellationSignal();
+
+    private void showFingerPrintDialog() {
+        try {
+
+            //AlertDialog.Builder builder = new AlertDialog.Builder(mAct);
+            AlertDialog.Builder builder = new AlertDialog.Builder(mAct);
+            //builder.setView(R.layout.fragment_fingerprint);
+            builder.setView(layout)
+            .setCancelable(false)
+            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    cancellationSignal.cancel();
+                    dialog.dismiss();
+                }
+            });
+            Dialog dialog = builder.create();
+            dialog.show();
+
+            mKeyStore.load(null);
+            SecretKey key = (SecretKey) mKeyStore.getKey(KEY_NAME, null);
+            Cipher lu = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                    + KeyProperties.BLOCK_MODE_CBC + "/"
+                    + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+            lu.init(Cipher.ENCRYPT_MODE, key);
+
+            mCryptoObj = new FingerprintManager.CryptoObject(lu);
+            FingerprintListener listener = new FingerprintListener(dialog, mCryptoObj);
+
+
+            mFingerPrintManager.authenticate(mCryptoObj, cancellationSignal,
+                   0, listener, null);
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.e(CLASS_TAG, Arrays.toString(e.getStackTrace()) + " " + e.getMessage());
+            Toast.makeText(mAct, e.getMessage() + " Dialog", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void createKey() {
+        try {
+            mKeyStore = KeyStore.getInstance("AndroidKeyStore");
+            mKeyStore.load(null);
+            mKeyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+            mKeyGenerator.init(new KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            mKeyGenerator.generateKey();
+
+        } catch (Exception e) {
+            Toast.makeText(mAct, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
 
